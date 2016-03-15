f6a41238ab8761f93726c6e27e7dd78c4f57ef65
mbelkin <michael@belkininteractive.com>
2016-03-14 22:59:34 -0700

    changed file names and such

---
+++ src/ios/MBGeofencing.h
@@ -0,0 +1,12 @@
+//
+//  MBGeofencing.h
+//
+//  Created by Michael Belkin on 3/14/16.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface MBGeofencing : CDVPlugin
+
+@end
---
+++ src/ios/MBGeofencing.m
@@ -0,0 +1,12 @@
+//
+//  MBGeofencing.m
+//
+//  Created by Michael Belkin on 3/14/16.
+//
+//
+
+#import "MBGeofencing.h"
+
+@implementation MBGeofencing
+
+@end
