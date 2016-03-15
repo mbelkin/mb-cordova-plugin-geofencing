67074cf678bd3b86a3c30a0b9107e432e48032a4
mbelkin <michael@belkininteractive.com>
2016-03-15 12:46:35 -0700

    Requesting necessary geofencing permissions

--- src/ios/MBGeofencing.m
+++ src/ios/MBGeofencing.m
@@ -7,6 +7,8 @@
 
 #import "MBGeofencing.h"
 
+#define	iOS8_OR_ABOVE								([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] != NSOrderedAscending)
+
 @interface MBGeofencing()<CLLocationManagerDelegate>
 
 @property (strong, nonatomic) CLLocationManager * locationManager;
@@ -19,7 +21,31 @@
 #pragma mark - Interface
 - (void)requestPermissions:(CDVInvokedUrlCommand *)command
 {
-
+	switch ([CLLocationManager authorizationStatus])
+	{
+		case kCLAuthorizationStatusRestricted:
+		case kCLAuthorizationStatusDenied:
+			//app is not permitted to use location services and you should abort your attempt to use them
+			if (command) [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"App is not permitted to use location services and you should abort your attempt to use them"] callbackId:command.callbackId];
+			break;
+		case kCLAuthorizationStatusNotDetermined:
+		case kCLAuthorizationStatusAuthorizedWhenInUse:
+			if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)])
+			{
+				// iOS 8
+				[self.locationManager requestAlwaysAuthorization];
+			}
+			else
+			{
+				// ios7
+				[self.locationManager startUpdatingLocation]; // do this just to promopt the permissions alert to show
+			}
+			if (command) [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Requested necessary geofencing permissions (but they're not yet granted as of the invocation of this callback)"] callbackId:command.callbackId];
+			break;
+		case kCLAuthorizationStatusAuthorized: // same as kCLAuthorizationStatusAuthorizedAlways in iOS8
+			if (command) [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Proper geofencing permissions are enabled"] callbackId:command.callbackId];
+			break;
+	}
 }
 
 - (void)startMonitoringRegion:(CDVInvokedUrlCommand *)command
@@ -37,36 +63,35 @@
 		return;
 	}
 	
-	// create the CLRegion
-	CLLocationCoordinate2D center = CLLocationCoordinate2DMake(lat, lon);
-	CLCircularRegion * region = [[CLCircularRegion alloc] initWithCenter:center radius:radius identifier:identifier];
-	region.notifyOnEntry = YES;
-	region.notifyOnExit = NO;
-	
 	// verify region monitoring availability and permissions
 	if (![CLLocationManager isMonitoringAvailableForClass:[CLCircularRegion class]])
 	{
 		[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Geofencing is not supported on this device!"] callbackId:callbackId];
 		return;
 	}
-	if ([CLLocationManager authorizationStatus] != kCLAuthorizationStatusAuthorizedAlways)
-	{
-		[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Region wass saved but will only be activated once user grants permission to access the device location."] callbackId:callbackId];
-		return;
-	}
 	
-	// all good, now try to add the region
-	if (!self.regionMonitoringCallbacks)
-	{
-		self.regionMonitoringCallbacks = [NSMutableDictionary new];
-	}
-	if (!self.regionMonitoringCallbacks[identifier])
-	{
-		self.regionMonitoringCallbacks[identifier] = [NSMutableArray new];
-	}
+	// all good, create the region and try to add it
+	CLLocationCoordinate2D center = CLLocationCoordinate2DMake(lat, lon);
+	CLCircularRegion * region = [[CLCircularRegion alloc] initWithCenter:center radius:radius identifier:identifier];
+	region.notifyOnEntry = YES;
+	region.notifyOnExit = NO;
 	
-	[self.regionMonitoringCallbacks[identifier] addObject:callbackId];
-	[self.locationManager startMonitoringForRegion:region];
+	if (![self geofencingPermissionsGranted])
+	{
+		[self requestPermissions:nil];
+		[self.locationManager startMonitoringForRegion:region];
+		[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Region was saved but will only be activated once user grants permission to access the device location (which was just asked for)"] callbackId:callbackId];
+	}
+	else
+	{
+		if (!self.regionMonitoringCallbacks[identifier])
+		{
+			self.regionMonitoringCallbacks[identifier] = [NSMutableArray new];
+		}
+		
+		[self.regionMonitoringCallbacks[identifier] addObject:callbackId];
+		[self.locationManager startMonitoringForRegion:region];
+	}
 }
 
 - (void)stopMonitoringRegion:(CDVInvokedUrlCommand *)command
@@ -155,36 +180,18 @@
 }
 
 #pragma mark - Helpers
-- (BOOL)isAuthorized
+- (BOOL)geofencingPermissionsGranted
 {
-    BOOL authorizationStatusClassPropertyAvailable = [CLLocationManager respondsToSelector:@selector(authorizationStatus)]; // iOS 4.2+
-
-    if (authorizationStatusClassPropertyAvailable) {
-        NSUInteger authStatus = [CLLocationManager authorizationStatus];
-#ifdef __IPHONE_8_0
-        if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {  //iOS 8.0+
-            return (authStatus == kCLAuthorizationStatusAuthorizedWhenInUse) || (authStatus == kCLAuthorizationStatusAuthorizedAlways) || (authStatus == kCLAuthorizationStatusNotDetermined);
-        }
-#endif
-        return (authStatus == kCLAuthorizationStatusAuthorized) || (authStatus == kCLAuthorizationStatusNotDetermined);
-    }
-
-    // by default, assume YES (for iOS < 4.2)
-    return YES;
-}
-
-- (BOOL)isLocationServicesEnabled
-{
-    BOOL locationServicesEnabledInstancePropertyAvailable = [self.locationManager respondsToSelector:@selector(locationServicesEnabled)]; // iOS 3.x
-    BOOL locationServicesEnabledClassPropertyAvailable = [CLLocationManager respondsToSelector:@selector(locationServicesEnabled)]; // iOS 4.x
-
-    if (locationServicesEnabledClassPropertyAvailable) { // iOS 4.x
-        return [CLLocationManager locationServicesEnabled];
-    } else if (locationServicesEnabledInstancePropertyAvailable) { // iOS 2.x, iOS 3.x
-        return [(id)self.locationManager locationServicesEnabled];
-    } else {
-        return NO;
-    }
+	switch ([CLLocationManager authorizationStatus])
+	{
+		case kCLAuthorizationStatusAuthorized: // same as kCLAuthorizationStatusAuthorizedAlways in iOS8
+			return YES;
+		case kCLAuthorizationStatusRestricted:
+		case kCLAuthorizationStatusDenied:
+		case kCLAuthorizationStatusNotDetermined:
+		case kCLAuthorizationStatusAuthorizedWhenInUse:
+			return NO;
+	}
 }
 
 - (void)logRegionEntry:(CLCircularRegion *)region
@@ -213,6 +220,15 @@
 }
 
 #pragma mark - Getters
+- (NSMutableDictionary *)regionMonitoringCallbacks
+{
+	if (!_regionMonitoringCallbacks)
+	{
+		_regionMonitoringCallbacks = [NSMutableDictionary new];
+	}
+	return _regionMonitoringCallbacks;
+}
+
 - (CLLocationManager *)locationManager
 {
 	if (!_locationManager)
--- www/geofencing.js
+++ www/geofencing.js
@@ -1,7 +1,48 @@
 /*global cordova, module*/
 
 module.exports = {
-    greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+	/**
+    * Requestes necessary geofencing permissions. 
+    * Calls success callback if permissions are already approved. 
+    * Else, it calls the error callback and passes a string indicating if persmissions were previously denied, or have not been asked for yet. 
+    * If the distinctions in the error callback are important, we can switch to returning an int or error code. 
+    *
+    * @param {Function} successCallback     The function to call when the position data is available
+    * @param {Function} errorCallback       The function to call when there is an error getting the heading position. (OPTIONAL)
+    */
+    requestPermissions:function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "MBGeofencing", "requestPermissions");
+    },
+    /**
+    * Starts monitoring a circular region
+    *
+    * @param {String} identifier            The identifier (or name) or the region for internal purposes
+    * @param {Float} lat                    The latitude of the region to monitor
+    * @param {Float} lon                    The longitude of the region to monitor
+    * @param {Float} radius                 The radius (in meters) of the region to monitor
+    * @param {Function} successCallback     The function to call when the position data is available
+    * @param {Function} errorCallback       The function to call when there is an error getting the heading position. (OPTIONAL)
+    */
+    startMonitoringRegion:function(identifier, lat, lon, radius, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "MBGeofencing", "startMonitoringRegion", [identifier, lat, lon, radius]);
+    },
+    /**
+    * Stops monitoring a circular region
+    *
+    * @param {String} identifier            The identifier (or name) or the region for internal purposes
+    * @param {Function} successCallback     The function to call when the position data is available
+    * @param {Function} errorCallback       The function to call when there is an error getting the heading position. (OPTIONAL)
+    */
+    stopMonitoringRegion:function(identifier, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "MBGeofencing", "stopMonitoringRegion", [identifier]);
+    },
+    /**
+    * Stops monitoring all previously registered regions
+    *
+    * @param {Function} successCallback     The function to call when the position data is available
+    * @param {Function} errorCallback       The function to call when there is an error getting the heading position. (OPTIONAL)
+    */
+    stopMonitoringAllRegions:function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "MBGeofencing", "stopMonitoringAllRegions", [identifier]);
     }
 };
